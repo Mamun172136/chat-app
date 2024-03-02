fe71d7c86c9a03c68c6e598ee6c3c7a065effa5d
Mamun172136 <ma1536204@gmail.com>
2024-03-02 20:58:57 +0600

    added signup controller and set cookies

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,12 @@
 import express from "express";
 import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
 import authRoutes from "./routes/auth.routes.js";
 import connectToMongoDB from "./db/connectionToMongoDB.js";
 const app = express();
 dotenv.config();
+app.use(express.json()); // to parse the incoming requests with JSON payloads (from req.body)
+app.use(cookieParser());
 
 app.get("/", () => {
   resizeBy.send("hello ");
