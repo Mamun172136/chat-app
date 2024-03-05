c896c5dd3219b3195c04d0669c68d04ffe3a19c4
Mamun172136 <ma1536204@gmail.com>
2024-03-05 12:29:14 +0600

    added get all user controller

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,8 @@ import cookieParser from "cookie-parser";
 import authRoutes from "./routes/auth.routes.js";
 import messageRoutes from "./routes/message.routes.js";
 import connectToMongoDB from "./db/connectionToMongoDB.js";
+import userRoutes from "./routes/user.routes.js";
+
 const app = express();
 dotenv.config();
 app.use(express.json()); // to parse the incoming requests with JSON payloads (from req.body)
@@ -15,6 +17,7 @@ app.get("/", () => {
 
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
+app.use("/api/users", userRoutes);
 
 app.listen(5000, () => {
   connectToMongoDB();
