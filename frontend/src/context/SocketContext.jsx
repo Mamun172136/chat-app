873b75126c88914cf9973ad2aa0d053d2ab3d242
Mamun172136 <ma1536204@gmail.com>
2024-03-11 05:59:52 +0600

    socket url updated

--- frontend/src/context/SocketContext.jsx
+++ frontend/src/context/SocketContext.jsx
@@ -16,7 +16,7 @@ export const SocketContextProvider = ({ children }) => {
 
   useEffect(() => {
     if (authUser) {
-      const socket = io("http://localhost:5000", {
+      const socket = io("https://chat-app-j0xs.onrender.com", {
         query: {
           userId: authUser._id,
         },
